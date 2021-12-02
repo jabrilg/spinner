cc795d2430ad7ee4f390ff6c27c802a9af1be59a
jabrilg <jabrilgadhane@hotmail.ca>
2021-12-02 21:24:54 +0000

    first comit

---
+++ spinner2.js
@@ -0,0 +1,14 @@
+let spins = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+
+for (let x = 0; x < spins.length; x++) {
+  if (x === spins.length - 1) {
+    setTimeout(() => {
+      process.stdout.write(`\r${spins[x]}\n`);
+    }, 400 + (x * 300));
+  } else {
+    setTimeout(() => {
+      process.stdout.write(`\r${spins[x]}   `);
+    }, 400 + (x * 300));
+  }
+}
+ 
